fca0f6625cbff840e396a5d25b4bece8bc264988
MrNades <156262559+rklauerWaltz@users.noreply.github.com>
2025-01-29 16:38:52 -0500

    Added movement boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,40 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private float hInput;
 
-    private void Start()
-    {
-        
-    }
-
+    public float xRange = 10;
 
   
     private void Update()
     {
         hInput = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime); 
+   
+
+        //Keep the player from moving beyond the xRange
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+            Debug.Log(xRange);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
     }
+
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Boundry")
+        {
+            Debug.Log("I'm hitting the left boundry");
+            transform.position = new Vector2(transform.position.x, transform.position.y);
+        }
+           
+    }*/
+
+    
 
 }
