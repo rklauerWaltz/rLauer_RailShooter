dbb57f5788be785abf4ead8976f89e7cb8af3b24
MrNades <156262559+rklauerWaltz@users.noreply.github.com>
2025-02-05 16:45:29 -0500

    Added Player Shooting

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float hInput;
+
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private PlayerShooting playerShoot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxisRaw("Horizontal");
+        playerMovement.Movement(hInput);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            playerShoot.FireBullet();
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,42 +6,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private float hInput;
-
     public float xRange = 10;
 
-  
-    private void Update()
-    {
-        hInput = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime); 
-   
+    public void Movement(float hInput)
+    {
+        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime);
+
 
         //Keep the player from moving beyond the xRange
-        if(transform.position.x < -xRange)
+        // looking at the player's position every frame. If it is less than or greater than the xRange we freeze the player by forcing a new position. 
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector2(-xRange, transform.position.y);
-            Debug.Log(xRange);
         }
-        if(transform.position.x > xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector2(xRange, transform.position.y);
         }
-
     }
-
-
-    /*private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Boundry")
-        {
-            Debug.Log("I'm hitting the left boundry");
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-        }
-           
-    }*/
-
-    
 
 }
